fca15e4d520f88069e87667cbf97cf2bce815365
gabrielvieira09 <gabrielafonsovieira09@gmail.com>
2024-09-20 09:04:14 -0300

    Criação e busca de Produto

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,13 +1,32 @@
-import { getAllProducts } from "../services/productService.js"
+import { createProduct, getAllProducts, getProductsById } from "../services/productService.js"
 
 export const getProducts = async(req, res) => {
    try {
       const products = await getAllProducts();
       res.json(products)
+   } catch (error) {
+      res.status(500).json({error:"Erro ao buscar produtos"});
    }
-   catch (error) {
-      res.status(500).json({
-         error:"Erro ao buscar produtos",
-      });
+}
+
+export const createProductController = async(req, res) => {
+   try{
+      const newProduct = await createProduct(req.body);
+      res.status(201).json(newProduct)
+   } catch (error) {
+      res.status(500).json ({error: "Erro ao criar produto"});
+   }
+}
+
+export const getProduct = async(req, res) => {
+   try{
+      const product = await getProductsById(req.params.id)
+      if(product){
+         res.json(product);
+      }else {
+         res.status(500).json({error: "Produto não encontrado"})
+      }
+   }catch (error){
+      res.status(500).json({error: "Erro ao buscar o produto"})
    }
 }
